2944e29e3141fa0341965961e16525c8540a3281
league <league@imac-16.attlocal.net>
2022-12-02 18:26:34 -0800

    i did the ninja star, greeter, madlibs, world domination and quiz game.

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,9 +8,13 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String input = JOptionPane.showInputDialog("do you know how to code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+if (input.equals("yes")){
+	JOptionPane.showMessageDialog(null,"then you can rule the world!");
+} else {
+	JOptionPane.showMessageDialog(null,"then sign up for classes at the league of amazing coders.");
+}
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
 	}
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,12 +1,42 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+int score = 0;		
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
+String input = JOptionPane.showInputDialog("is a house big");	
+System.out.println(input);
+if (input.equals("yes")) {
+	score = score + 1;
+}else {
+	score = score - 1;
+}
+System.out.println(score);
+String input2 = JOptionPane.showInputDialog("is a house small");	
+System.out.println(input2);
+if (input2.equals("no")) {
+	score = score + 1;
+}else {
+	score = score - 1;
+}
+System.out.println(score);
+
+
+
+String input3 = JOptionPane.showInputDialog("is a elephant big");	
+System.out.println(input3);
+if (input3.equals("yes")) {
+	score = score + 1;
+}else {
+	score = score - 1;
+}
+System.out.println(score);
+
+JOptionPane.showMessageDialog(null,score);
 				// 2.  Ask the user a question 
 				
 				// 3.  Use an if statement to check if their answer is correct
